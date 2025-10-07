ed00e0a4dd160d097a17cb1b23447e63bbe1ba9b
FcoHdez1404 <franciscoescom42@gmail.com>
2025-10-07 17:42:14 -0600

    cambio de boton a flechas

--- use_cases/chat_gpt_agent.py
+++ use_cases/chat_gpt_agent.py
@@ -63,7 +63,7 @@ if "show_col2" not in st.session_state:
     st.session_state.show_col2 = True
 
 # Botón con ícono para mostrar/ocultar col2
-icon_svg = '''<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="32" height="32" rx="16" fill="#1E90FF"/><path d="M16 10V22" stroke="white" stroke-width="2" stroke-linecap="round"/><path d="M10 16H22" stroke="white" stroke-width="2" stroke-linecap="round"/></svg>'''
+icon_svg = '''<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="arrow-show_hide.png"><rect width="32" height="32" rx="16" fill="#1E90FF"/><path d="M16 10V22" stroke="white" stroke-width="2" stroke-linecap="round"/><path d="M10 16H22" stroke="white" stroke-width="2" stroke-linecap="round"/></svg>'''
 hide_icon_svg = '''<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="32" height="32" rx="16" fill="#1E90FF"/><rect x="10" y="15" width="12" height="2" rx="1" fill="white"/></svg>'''
 
 col1.markdown("<div style='display: flex; justify-content: flex-end;'>", unsafe_allow_html=True)
